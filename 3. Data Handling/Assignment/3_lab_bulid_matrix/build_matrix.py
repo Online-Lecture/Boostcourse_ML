03cc70df8a9a7660f353b3ac2891471951969856
ameliacode <17011663@sju.ac.kr>
2020-07-14 18:19:31 +0900

    Update Chapter 3

---
+++ 3. Data Handling/Assignment/3_lab_bulid_matrix/build_matrix.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+
+
+def get_rating_matrix(filename, dtype=np.float32):
+    df = pd.read_csv(filename)
+    return df.groupby(["source", "target"])["rating"].sum().unstack().fillna(0)
+
+def get_frequent_matrix(filename, dtype=np.float32):
+    df = pd.read_csv(filename)
+    df["rating"] = 1
+    return df.groupby(["source", "target"])["rating"].sum().unstack().fillna(0)
